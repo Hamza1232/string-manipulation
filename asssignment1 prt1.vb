e38850fb7954e2fbd35864558898e15c9a5b920e
Hamza1232 <56012288+Hamza1232@users.noreply.github.com>
2019-10-13 11:22:48 -0700

    Add files via upload

---
+++ asssignment1 prt1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2 As String
+        str1 = ""
+        str2 = ""
+        Console.WriteLine("enter the first string")
+        str1 = Console.ReadLine
+        Console.WriteLine("enter second string")
+        str2 = Console.ReadLine
+        Console.Write("final string is " & str1 & str2)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
