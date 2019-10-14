e269ce09e69329e459291aef8a270ce033a34c0f
Hamza1232 <56012288+Hamza1232@users.noreply.github.com>
2019-10-14 09:23:32 -0700

    Add files via upload
    inverse a string

---
+++ assignment part 5.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2, nextchar As String
+        Dim count As Integer
+        str1 = ""
+        str2 = ""
+        nextchar = ""
+        count = 0
+        Console.Write("kindly enter the string")
+        str1 = Console.ReadLine
+        For count = 1 To Len(str1)
+            nextchar = Right(str1, count)
+
+            str2 = str2 + nextchar
+
+
+        Next
+
+        Console.WriteLine("final string is " & str2)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
+
